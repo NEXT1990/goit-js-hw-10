1da1e40b894be79f128eb342d065bddb60e99915
NEXT1990 <maksim.listunov@gmail.com>
2022-12-15 00:46:52 +0200

    add-hw-10

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+
+const inputField = document.getElementById('search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+console.dir(countryInfo);
+
+inputField.addEventListener(
+  'keydown',
+  debounce(event => {
+    GetCountries({
+      name: inputField.value,
+    });
+  }, DEBOUNCE_DELAY)
+);
+
+function GetCountries({ name }) {
+  const urlApi = `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`;
+  fetch(urlApi)
+    .then(res => {
+      if (res.status !== 200) {
+        console.log('Все что угодно, но не 200!');
+      }
+      return res.json();
+    })
+    .then(data => {
+      render(data);
+      console.log(data);
+    });
+}
+
+function render(countryData) {
+  countryInfo.innerHTML = '';
+  countryData.forEach(({ name, capital, population, flag, languages }) => {
+    const DataEl = `
+      < img src = "${flag}" alt = "${name}" />
+      <h1>${name}</h1>
+      <p>${capital}</p>
+      <p>${population}</p>
+      <p>${languages}</p>
+      `;
+
+    countryInfo.insertAdjacentHTML('beforeend', DataEl);
+  });
+}
